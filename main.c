895db4886f5388b013c4851bf4c5298765c38cac
ishansingh7 <84373105+ishansingh7@users.noreply.github.com>
2024-02-21 15:23:15 +0530

    odd and even of array
    odd and even of array

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int n, sum = 0;
-
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-
-    int *arr = (int *)malloc(n * sizeof(int));
-
-    if (arr == NULL) {
-        printf("Memory allocation failed\n");
-        return 1;
+int main(void) {
+  int n;
+  printf("Enter the number of elements in the array: ");
+  scanf("%d", &n);
+  int *array = (int *)malloc(n * sizeof(int));
+  printf("enter the elements:");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &array[i]);
+  }
+  printf("Enter the elements odd:\n");
+  for (int i = 0; i < n; i++) {
+    if (array[i] % 2 != 0) {
+      printf("%d\n", array[i]);
     }
-
-    printf("Enter the elements of the array:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
+  }
+  printf("Enter the elements even:\n");
+  for (int i = 0; i < n; i++) {
+    if (array[i] % 2 == 0) {
+      printf("%d\n", array[i]);
     }
+  }
 
-    printf("The sum of the array elements is: %d\n", sum);
-
-    free(arr);
-
-    return 0;
+  return 0;
 }
